b563e42d4fd925ea427621ce33e754a11a3aa0e0
Stan97-2 <stan.dellon972@gmail.com>
2024-01-22 23:01:56 +0100

    add: login views

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,5 +1,11 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import LoginView from '../views/LoginView.vue'
+import LoginAcademicTutorView from '../views/LoginAcademicTutorView.vue'
+import LoginAdminView from '../views/LoginAdminView.vue'
+import LoginCompanyTutorView from '../views/LoginCompanyTutorView.vue'
+import LoginStudentView from '../views/LoginStudentView.vue'
+
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -9,6 +15,31 @@ const router = createRouter({
       name: 'home',
       component: HomeView
     },
+    {
+      path: '/login',
+      name: 'login',
+      component: LoginView
+    },
+    {
+      path: '/login/academic-tutor',
+      name: 'login-academic-tutor',
+      component: LoginAcademicTutorView
+    },
+    {
+      path: '/login/admin',
+      name: 'login-admin',
+      component: LoginAdminView
+    },
+    {
+      path: '/login/company-tutor',
+      name: 'login-company-tutor',
+      component: LoginCompanyTutorView
+    },
+    {
+      path: '/login/student',
+      name: 'login-student',
+      component: LoginStudentView
+    }
   ]
 })
 
--- frontend/src/views/LoginView.vue
+++ frontend/src/views/LoginView.vue
@@ -1,21 +1,26 @@
 <template>
-    <div class="button-container">
-        <button class="button">
-            <i class="icon"></i>
-            <span class="text">Button 1</span>
-        </button>
-        <button class="button">
-            <i class="icon"></i>
-            <span class="text">Button 2</span>
-        </button>
-        <button class="button">
-            <i class="icon"></i>
-            <span class="text">Button 3</span>
-        </button>
-        <button class="button">
-            <i class="icon"></i>
-            <span class="text">Button 4</span>
-        </button>
+    <div>
+        <div class="button-container">
+            <div>
+                <h2 class="title">Vous êtes : </h2>
+            </div>
+            <RouterLink to="/login/student" class="button">
+                <i class="icon"></i>
+                <span class="text">Élève</span>
+            </RouterLink>
+            <RouterLink to="/login/academic-tutor" class="button">
+                <i class="icon"></i>
+                <span class="text">Tuteur École</span>
+            </RouterLink>
+            <RouterLink to="/login/company-tutor" class="button">
+                <i class="icon"></i>
+                <span class="text">Tuteur Entreprise</span>
+            </RouterLink>
+            <RouterLink to="/login/admin" class="button">
+                <i class="icon"></i>
+                <span class="text">Administrateur</span>
+            </RouterLink>
+        </div>
     </div>
 </template>
 
@@ -36,27 +41,42 @@ export default {
 
 <style scoped>
 .button-container {
-    width: 200px;
-    height: 400px;
+    margin: 0 auto;
+    margin-top: 50px;
+    width: 600px;
     display: flex;
     flex-direction: column;
+    align-items: center;
+    
 }
 
 .button {
     display: flex;
     align-items: center;
     padding: 10px;
-    margin-bottom: 10px;
+    margin: 10px;
     background-color: #f0f0f0;
     border: none;
-    border-radius: 5px;
+    border-radius: 10px;
+    width: 90%;
+    height: 64px;
+    text-decoration: none;
+    color: #333232;
+    box-shadow: 2px 2px 2px rgba(0, 0, 0, 0.2);
 }
 
 .icon {
     margin-right: 10px;
 }
 
+.title {
+    font-size: 25px;
+    font-family: Verdana, sans-serif;
+}
+
 .text {
-    font-size: 14px;
+    font-size: 16px;
+    font-family: Verdana, sans-serif;
 }
 </style>
+    
