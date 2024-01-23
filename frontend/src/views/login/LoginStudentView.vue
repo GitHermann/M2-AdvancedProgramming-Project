f1ad824981b2a430971d95481e111c48de0d0495
Stan97-2 <stan.dellon972@gmail.com>
2024-01-23 15:37:50 +0100

    refactor login views

---
+++ frontend/src/views/login/LoginStudentView.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="login-form-container">
+        <LoginForm @submit="login" />
+    </div>
+</template>
+  
+<script>
+import LoginForm from '@/components/LoginForm.vue';
+  
+export default {
+    components: {
+      LoginForm,
+    },
+    methods: {
+      login(userData) {
+        console.log('Company tutor Login:', userData);
+      },
+    },
+};
+</script>
+  
+<style scoped>
+
+.login-form-container {
+    margin: 0 auto;
+    margin-top: 50px;
+    width: 600px;
+}
+
+</style>
