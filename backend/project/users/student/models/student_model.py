a04fe15cb470d62eccd6551d2784f810480a61fd
Benjamin Bernard <bartowsky5@live.com>
2024-01-23 11:41:53 +0100

    Implementation student login and added all attributes

--- backend/project/users/student/models/student_model.py
+++ backend/project/users/student/models/student_model.py
@@ -1,6 +1,6 @@
 import bcrypt
 
-from project.app import app, database_client
+from project.app import database_client
 
 students_collection = database_client["Project"]["students"]
 
@@ -9,15 +9,29 @@ class Student:
     @staticmethod
     def create_student(data):
 
-        existing_user = students_collection.find_one({'nom_utilisateur': data['nom_utilisateur']})
-        if existing_user:
-            return {'message': 'Nom d\'utilisateur déjà utilisé'}, 400
+        existing_user_student = students_collection.find_one({'email': data['email']})
+        if existing_user_student:
+            return {'message': 'Student already exists'}, 400
 
-        hashed_password = bcrypt.hashpw(data['mot_de_passe'].encode('utf-8'), bcrypt.gensalt())
-        nouvel_utilisateur = {
-            'nom_utilisateur': data['nom_utilisateur'],
-            'adresse_email': data['adresse_email'],
-            'mot_de_passe': hashed_password
+        hashed_password = bcrypt.hashpw(data['password'].encode('utf-8'), bcrypt.gensalt())
+        new_student = {
+            'first_name': data['first_name'],
+            'last_name': data['last_name'],
+            'email': data['email'],
+            'student_id': data['student_id'],
+            'password': hashed_password,
+            'phone': data['phone'],
+            'promotion': data['promotion']
         }
-        students_collection.insert_one(nouvel_utilisateur)
-        return {'message': 'Inscription réussie'}
+        students_collection.insert_one(new_student)
+        return {'message': 'Sign In successful', 'user': new_student}, 201
+
+    @staticmethod
+    def student_log_in(data):
+        existing_user_student = students_collection.find_one(
+            {'email': data['email']})
+        if existing_user_student and bcrypt.checkpw(data['password'].encode('utf8'),existing_user_student['password']):
+            return {'message': 'Login successful', 'code': 201}
+        else:
+            return {'message': 'Invalid email or password', 'code': 401}
+
--- backend/project/users/student/routes/student_routes.py
+++ backend/project/users/student/routes/student_routes.py
@@ -4,16 +4,31 @@ from project.app import app
 from project.users.student.models.student_model import Student
 from __main__ import app
 
-@app.route('/user/student/sign_in', methods=['POST'])
+
+@app.route('/user/student/signin', methods=['POST'])
 def student_sign_in():
     try:
         data = request.json
         response = Student.create_student(data)
-        return jsonify({"message": "users added successfully", "user_id": str(response)}), 201
+        return jsonify({"message": str(response)}), 201
 
     except Exception as e:
         return jsonify({"error": str(e)}), 500
 
+
+@app.route('/user/student/login', methods=['POST'])
+def student_log_in():
+    try:
+        data = request.json
+        response = Student.student_log_in(data)
+
+        if response['code'] == 201:
+            return jsonify({"message": response['message']}), 201
+        else:
+            return jsonify({"message": response['message']}), 401
+
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
 
 @app.route('/user/student', methods=['POST'])
 def add_user_student():
