66a3b8e0522df463f0595d383af59b71ee26649d
GitHermann <hermann.huang@efrei.net>
2024-01-24 15:52:45 +0100

    intership model and add internship route

--- backend/project/internships/routes/internship_routes.py
+++ backend/project/internships/routes/internship_routes.py
@@ -1,9 +1,17 @@
+from flask import request, jsonify
+
 from project.app import app
+from project.internships.models.internship_model import Internship
 
 
 @app.route('/internships', methods=['POST'])
 def add_internship():
-    return 'Hello World!'
+    try:
+        data = request.json
+        response = Internship.createInternship(data)
+        return jsonify({"message": "Internship added successfully"}), 201
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
 
 
 @app.route('/internships/<id>', methods=['GET'])
