fec7f9da15eaeb5cc4d0f1af9e6b7653d67d9f55
Your Name <fcharvet0901@gmail.com>
2024-02-05 11:39:24 +0100

    updates route redirection in vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -15,6 +15,24 @@ export default defineConfig({
   },
   server: {
     host: '0.0.0.0',
+    proxy: {
+      '/internships': {
+        target: 'http://localhost:5001',
+        changeOrigin: true
+      },
+      '/internship_spaces': {
+        target: 'http://localhost:5002',
+        changeOrigin: true
+      },
+      '/user': {
+        target: 'http://localhost:5003',
+        changeOrigin: true
+      },
+      '/documents': {
+        target: 'http://localhost:5004',
+        changeOrigin: true
+      }
+    },
     port: 8082
   },
 })
